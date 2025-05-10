7d8b92f75790320901c60d29ef735a0c462e67f9
KodeKenobi <kodekenobi@gmail.com>
2025-05-10 20:58:55 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kodekenobi.github.io",
+  base: "/kodekenobi.github.io/",
   build: {
     outDir: "dist",
     sourcemap: true,
