ae79d25d74226a260947c885782e5d5f14653716
KodeKenobi <kodekenobi@gmail.com>
2025-05-10 21:40:56 +0200

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./App.tsx";
-import Home from "./components/Home.tsx";
-import About from "./components/AboutPage.tsx";
-import SkillsPage from "./components/SkillsPage.tsx";
-import ProjectsPage from "./components/ProjectsPage.tsx";
-import NusuruPage from "./components/NusuruPage.tsx";
 
-console.log("🚀 main.tsx: Starting React application initialization");
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+        fontWeight: "bold",
+      }}
+    >
+      Hello World
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/kodekenobi.github.io">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<SkillsPage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/projects/nusuru" element={<NusuruPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
-
-console.log("✅ main.tsx: React application initialized");
