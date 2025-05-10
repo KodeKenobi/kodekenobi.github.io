0826bcc91127a102aebb1a8bfea6d3aadb8e99bd
KodeKenobi <kodekenobi@gmail.com>
2025-05-10 20:02:46 +0200

    major haul

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kodekenobi.github.io/",
+  base: "/kodekenobi.github.io",
   build: {
     outDir: "dist",
     assetsDir: "assets",
