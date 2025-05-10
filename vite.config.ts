46e8629cf5f85608102cf50283337cec9a3eef84
KodeKenobi <kodekenobi@gmail.com>
2025-05-10 18:45:57 +0200

    major haul

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
     sourcemap: true,
     assetsDir: "assets",
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
       output: {
         manualChunks: undefined,
         assetFileNames: "assets/[name].[hash][extname]",
@@ -20,5 +24,8 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    strictPort: true,
+    host: true,
   },
+  publicDir: "public",
 });
