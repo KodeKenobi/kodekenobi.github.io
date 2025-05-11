90386abc587ae75d0f6fdb473acbec4e138a43ad
KodeKenobi <kodekenobi@gmail.com>
2025-05-11 09:30:33 +0200

    Fix about us navbar

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,4 +1,4 @@
-import { FaLinkedin } from "react-icons/fa";
+import { FaLinkedin, FaDownload } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -19,8 +19,8 @@ const About = () => {
           download
           className="flex items-center space-x-2 px-4 py-2 bg-gray-700 text-gray-200 rounded-lg hover:bg-gray-600 transition-colors duration-200"
         >
-          <FaLinkedin className="text-xl" />
-          <span>Download CV</span>
+          <FaDownload className="text-xl" />
+          <span>My CV</span>
         </a>
       </div>
 
