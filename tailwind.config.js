db3e9ea0f2e25ab1557dc06215c33ce9315de769
KodeKenobi <kodekenobi@gmail.com>
2025-06-27 14:33:25 +0200

    Push all local changes to new branch my-fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
-      },
-      boxShadow: {
-        card: "0 35px 120px -15px #211e35",
-      },
-      screens: {
-        xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": `url(/herobg.png)`,
+        "terminal-red": "#ff5f56",
+        "terminal-yellow": "#ffbd2e",
+        "terminal-green": "#27c93f",
       },
     },
   },
